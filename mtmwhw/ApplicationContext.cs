9c11de4f49110588e4cc5f499c87aa6d77ca6a3b
whowher <116297463+whowher@users.noreply.github.com>
2022-10-23 21:24:45 +0300

    Add files via upload

---
+++ mtmwhw/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mtmwhw;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=mtmwhw.db");
+    }
+}
---
+++ mtmwhw/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mtmwhw;
+using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            Console.WriteLine("Input Name:");
+            string lexa = Console.ReadLine();
+            Console.WriteLine("Input Age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            User Lexa = new User { Name = lexa, Age = age };
+            db.Users.Add(Lexa);
+            db.SaveChanges();
+            var users = db.Users.ToList();
+            Console.WriteLine("Object Added");
+            Console.WriteLine("Список объектов:");
+            foreach (User u in users)
+            {
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+            }
+
+        }
+    }
+}
